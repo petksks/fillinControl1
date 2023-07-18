cc5aa39c54419c6e6e10da6970e918a8ca95e615
Peter K.Sederblad <peter.sedberg@gmail.com>
2023-07-18 15:05:15 +0200

    Initial Commit

---
+++ fillinControl1/fillinControl1App.swift
@@ -0,0 +1,17 @@
+//
+//  fillinControl1App.swift
+//  fillinControl1
+//
+//  Created by User on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct fillinControl1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fillinControl1UITests/fillinControl1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  fillinControl1UITestsLaunchTests.swift
+//  fillinControl1UITests
+//
+//  Created by User on 7/18/23.
+//
+
+import XCTest
+
+final class fillinControl1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
