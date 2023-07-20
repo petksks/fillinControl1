5dae835ed5ac722649f41c0e432efc40170320b4
Peter K.Sederblad <peter.sedberg@gmail.com>
2023-07-20 15:44:13 +0200

    fix button size

--- fillinControl1/ContentView.swift
+++ fillinControl1/ContentView.swift
@@ -1,4 +1,6 @@
 import SwiftUI
+// inspo: https://dribbble.com/shots/5527963-Simple-Gym-App-Ui-Main-Menu
+
 
 struct ContentView: View {
     var body: some View {
@@ -11,42 +13,66 @@ struct ContentView: View {
                         .foregroundColor(.black)
                         .padding([.leading, .top, .bottom])
                         .bold(true)
-                    
-                    Spacer()
+                        Spacer()
                 }
                 
                 //App name
-                Text("Fillin Control")
-                    .font(.largeTitle)
-                    .foregroundColor(.black)
-                    .padding([.top, .bottom])
-                    .bold(true)
+                Text("fillin control")
+                    .font(.custom("HelveticaNeue", size: 44, relativeTo: .largeTitle))
+                        .foregroundColor(.black)
+                        .padding([.top, .bottom])
+                        .fontWeight(.bold)
             }
             
-            VStack {
+            VStack(spacing: 0){
                 Button("Menu item 1") {
-//                        .frame(maxWidth: .infinity)
+                    // button action
                 }
+                .buttonStyle(CustomButtonStyle(color: .red))
+//                .background(Color.yellow)
                 
                 Button("Menu item 2") {
                     print("Menu item 2 tapped")
                 }
+                .buttonStyle(CustomButtonStyle(color: .blue))
+                .background(Color.blue)
                 
                 Button("Menu item 3") {
                     print("Menu item 3 tapped")
                 }
+                .buttonStyle(CustomButtonStyle(color: .purple))
+                .background(Color.orange)
+                
                 Button("Menu item 4"){
                     print("Menu item 4 tapped")
                 }
+                .buttonStyle(CustomButtonStyle(color: .orange))
+                .background(Color.green)
+                Spacer()
+            
             }
             
             Spacer() // This will push your buttons up
         }
         .background(Color.white)
-        Color.white.edgesIgnoringSafeArea(.all)
+//        Color.white.edgesIgnoringSafeArea(.all)
         
                   }
     }
+
+struct CustomButtonStyle: ButtonStyle {
+    var color: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .foregroundColor(.white)
+            .bold(true)
+            .background(color)
+            .font(.title)
+            
+    }
+}
 
 
 struct ContentView_Previews: PreviewProvider {
