9efb8968a8ba98ebdbc03b2ad9185e3374fe88d8
Peter K.Sederblad <peter.sedberg@gmail.com>
2023-07-20 14:17:11 +0200

    fix title of main menu

--- fillinControl1/ContentView.swift
+++ fillinControl1/ContentView.swift
@@ -1,23 +1,53 @@
-//
-//  ContentView.swift
-//  fillinControl1
-//
-//  Created by User on 7/18/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ZStack {
+                HStack {
+                    // Navbar icon
+                    Image(systemName: "list.bullet")
+                        .font(.title)
+                        .foregroundColor(.black)
+                        .padding([.leading, .top, .bottom])
+                        .bold(true)
+                    
+                    Spacer()
+                }
+                
+                //App name
+                Text("Fillin Control")
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                    .padding([.top, .bottom])
+                    .bold(true)
+            }
+            
+            VStack {
+                Button("Menu item 1") {
+//                        .frame(maxWidth: .infinity)
+                }
+                
+                Button("Menu item 2") {
+                    print("Menu item 2 tapped")
+                }
+                
+                Button("Menu item 3") {
+                    print("Menu item 3 tapped")
+                }
+                Button("Menu item 4"){
+                    print("Menu item 4 tapped")
+                }
+            }
+            
+            Spacer() // This will push your buttons up
         }
-        .padding()
+        .background(Color.white)
+        Color.white.edgesIgnoringSafeArea(.all)
+        
+                  }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
